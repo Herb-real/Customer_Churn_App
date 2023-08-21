7bb69f872b2cdb10506ee75ab16474c0967cfe86
Herbert Djanie <hdjaniejnr@gmail.com>
2023-08-21 11:01:12 +0000

    Gradio

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-import joblib 
+import joblib
 import pandas as pd
 import numpy as np
 import gradio as gr
@@ -13,37 +13,22 @@ from sklearn.utils.class_weight import compute_class_weight
 import gradio as gr
 import joblib
 import warnings
-import pickle
-import os
+
 
 warnings.filterwarnings("ignore")
 
-def loadml_components(fp):
-    'loading model'
-    with open(fp, 'rb') as f:
-        object = pickle.load(f)
-    return object
+model = joblib.load("models/LR.joblib")
 
-# Setup
-## Variables and constants
-_file_ = "src"
-DIRPATH = os.path.dirname(os.path.realpath(_file_))  
-fp = os.path.join(DIRPATH, "assets", "ml", "ml.pkl")
+model
 
-## Execution
-# ml_components_dict = loadml_components(fp=ml_core_fp)
+test = pd.read_csv("dataframes/Vodafone_churn.csv")
+test
 
-# model= joblib.load("models/LR.joblib")
+# testing our model
+model.predict(test)
 
-# model
+# creating a function to return a string depending on the output of the model
 
-# test= pd.read_csv("dataframes/Vodafone_churn.csv")
-# test
-
-# ##testing our model
-# model.predict(test)
-
-##creating a function to return a string depending on the output of the model
 
 def classify(num):
     if num == 0:
@@ -59,18 +44,17 @@ defining my parameters which my fucntion will accept, and are the same as the fe
 def predict_churn(SeniorCitizen, Partner, Dependents, tenure, InternetService,
                   OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport,
                   StreamingTV, StreamingMovies, Contract, PaperlessBilling,
-                  PaymentMethod, MonthlyCharges, TotalCharges): 
+                  PaymentMethod, MonthlyCharges, TotalCharges):
 
-     
-     ##in the code below, I am created a list of my input features
+    # in the code below, I am created a list of my input features
 
     input_data = [
         SeniorCitizen, Partner, Dependents, tenure, InternetService,
         OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport,
         StreamingTV, StreamingMovies, Contract, PaperlessBilling,
         PaymentMethod, MonthlyCharges, TotalCharges
-    ]    
-##I am changing my features into a dataframe since that is how I trained my model
+    ]
+# I am changing my features into a dataframe since that is how I trained my model
 
     input_df = pd.DataFrame([input_data], columns=[
         "SeniorCitizen", "Partner", "Dependents", "tenure", "InternetService",
@@ -79,56 +63,71 @@ def predict_churn(SeniorCitizen, Partner, Dependents, tenure, InternetService,
         "PaymentMethod", "MonthlyCharges", "TotalCharges"
     ])
 
+    # I am making a prediction on the input data.
+    pred = model.predict(input_df)
 
-    # pred = model.predict(input_df) ##I am making a prediction on the input data.
-
-    # output = classify(pred[0]) ## I am passing the first predction through my classify function I created earlier
+    # I am passing the first predction through my classify function I created earlier
+    output = classify(pred[0])
 
     if output == "Customer will not Churn":
         return [(0, output)]
     else:
-        return [(1, output)]   ##setting my function to return the binary classification and the written output
+        # setting my function to return the binary classification and the written output
+        return [(1, output)]
+
 
 output = gr.outputs.HighlightedText(color_map={
     "Customer will not Churn": "green",
     "Customer will churn": "red"
-}) ##assigning colors to the respective output 
+})  # assigning colors to the respective output
 
-##building my interface and wrapping my model in the function
+# building my interface and wrapping my model in the function
 
-##using gradio blocks to beautify my output
+# using gradio blocks to beautify my output
 
-block= gr.Blocks(theme= "freddyaboulton/dracula_revamped") ##instatiating my blocks class
+# instatiating my blocks class
+block = gr.Blocks()
 
 with block:
-    gr.Markdown(""" # Welcome to App for Customer Churn Prediction """)
-    
-    input=[gr.inputs.Slider(minimum=0, maximum= 1, step=1, label="SeniorCitizen: Select 1 for Yes and 0 for No"),
-        gr.inputs.Radio(["Yes", "No"], label="Partner: Do You Have a Partner?"),
-        gr.inputs.Radio(["Yes", "No"], label="Dependents: Do You Have a Dependent?"),
-        gr.inputs.Number(label="tenure: How Long Have You Been with Vodafone in Months?"),
-        gr.inputs.Dropdown(["DSL", "Fiber optic", "No"], label="What Internet Service Do You Use?"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="Do You Have Online Security?"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="Do You Have Any Online Backup Service?"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="Do You Use Any Device Protection?"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="Do You Use TechSupport?"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="Do You Stream TV?"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="Do You Stream Movies?"),
-        gr.inputs.Dropdown(["Month-to-month", "One year", "Two year"], label="What Is Your Contract Type?"),
-        gr.inputs.Dropdown(["Yes", "No"], label=" Do You Use Paperless Billing?"),
-        gr.inputs.Dropdown([
-            "Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"
-        ], label="What Payment Method Do You Use?"),
-        gr.inputs.Number(label="What is you Monthly Charges?"),
-        gr.inputs.Number(label="How Much Is Your Total Charges?")]
-     
-    output= gr.outputs.HighlightedText(color_map={
-     "Customer will not Churn": "green",
-     "Customer will churn": "red"}, label= "Your Output")     
-    predict_btn= gr.Button("Predict")
-     
-    predict_btn.click(fn= predict_churn, inputs= input, outputs=output)
+    gr.Markdown(""" # Welcome to My Customer Churn Prediction App""")
+
+    input = [gr.inputs.Slider(minimum=0, maximum=1, step=1, label="SeniorCitizen: Select 1 for Yes and 0 for No"),
+             gr.inputs.Dropdown(
+                 ["Yes", "No"], label="Partner: Do You Have a Partner?"),
+             gr.inputs.Dropdown(
+                 ["Yes", "No"], label="Dependents: Do You Have a Dependent?"),
+             gr.inputs.Number(
+                 label="tenure: How Long Have You Been with Vodafone in Months?"),
+             gr.inputs.Dropdown(["DSL", "Fiber optic", "No"],
+                                label="What Internet Service Do You Use?"),
+             gr.inputs.Dropdown(["Yes", "No", "No internet service"],
+                                label="Do You Have Online Security?"),
+             gr.inputs.Dropdown(["Yes", "No", "No internet service"],
+                                label="Do You Have Any Online Backup Service?"),
+             gr.inputs.Dropdown(["Yes", "No", "No internet service"],
+                                label="Do You Use Any Device Protection?"),
+             gr.inputs.Dropdown(["Yes", "No", "No internet service"],
+                                label="Do You Use TechSupport?"),
+             gr.inputs.Dropdown(["Yes", "No", "No internet service"],
+                                label="Do You Stream TV?"),
+             gr.inputs.Dropdown(["Yes", "No", "No internet service"],
+                                label="Do You Stream Movies?"),
+             gr.inputs.Dropdown(["Month-to-month", "One year",
+                                 "Two year"], label="What Is Your Contract Type?"),
+             gr.inputs.Dropdown(
+                 ["Yes", "No"], label=" Do You Use Paperless Billing?"),
+             gr.inputs.Dropdown([
+                 "Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"
+             ], label="What Payment Method Do You Use?"),
+             gr.inputs.Number(label="What is you Monthly Charges?"),
+             gr.inputs.Number(label="How Much Is Your Total Charges?")]
+
+    output = gr.outputs.HighlightedText(color_map={
+        "Customer will not Churn": "green",
+        "Customer will churn": "red"}, label="Your Output")
+    predict_btn = gr.Button("Predict")
+
+    predict_btn.click(fn=predict_churn, inputs=input, outputs=output)
 
 block.launch()
-    
 
